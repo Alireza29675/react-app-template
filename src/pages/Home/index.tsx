1d5d484770d625a163e07b1de0a63895278d2cc7
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2020-05-20 15:30:52 +0430

    better beginning

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,30 @@ import { RouteComponentProps } from "@reach/router"
 import css from "./style.module.scss"
 
 export default class extends React.Component<RouteComponentProps> {
+  state = {
+    i: 0,
+  }
+  componentDidMount() {
+    setInterval(this.nextSlug.bind(this), 100)
+    this.nextSlug()
+  }
+  nextSlug() {
+    this.setState({
+      i: (this.state.i + 1) % 3,
+    })
+  }
+
   render() {
-    return <div className={css.container}></div>
+    const { i } = this.state
+
+    return (
+      <div className={css.container}>
+        <h1>
+          <span style={{ opacity: i === 0 ? 1 : 0 }}>REACT </span>
+          <span style={{ opacity: i === 1 ? 1 : 0 }}>IS</span>
+          <span style={{ opacity: i === 2 ? 1 : 0 }}>READY</span>
+        </h1>
+      </div>
+    )
   }
 }
--- src/stores/noopStore.ts
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux"
-
-const store = createStore(() => 0)
-
-export default store
